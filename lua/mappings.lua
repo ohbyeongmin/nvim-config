ea679b7748e8872817aa038d4b9b262973b3504b
ohbyeongmin <fbottlem@gmail.com>
2024-07-24 11:35:40 +0900

    mapping: split window mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
