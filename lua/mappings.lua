63b88063f6e4e1cc4cf7c90c7ec6af7352a2ad94
ohbyeongmin <fbottlem@gmail.com>
2024-09-05 15:14:29 +0900

    config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,7 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })   -- split window vertically
 map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
@@ -18,3 +18,13 @@ map("n", "<C-u>", "<C-u>zz")
 
 map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
+
+map("n", "sh", "<C-w>h")
+map("n", "sk", "<C-w>k")
+map("n", "sj", "<C-w>j")
+map("n", "sl", "<C-w>l")
+
+map("n", "ss", ":split<Return>")
+
+-- telescope todo list
+map("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todos" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,6 +11,7 @@ return {
     opts = {
       ensure_installed = {
         "eslint-lsp",
+        "eslint_d",
         "prettierd",
         "tailwindcss-language-server",
         "typescript-language-server",
@@ -55,9 +56,55 @@ return {
   },
   {
     "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
+    event = { "BufReadPre", "BufNewFile" }, -- uncomment for format on save
     config = function()
-      require "configs.conform"
+      local conform = require "conform"
+
+      conform.setup {
+        formatters_by_ft = {
+          go = { "goimports", "gofmt" },
+          rust = { "rustfmt", lsp_format = "fallback" },
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
+          css = { "prettier" },
+          html = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+        },
+        format_on_save = {
+          lsp_format = "fallback",
+        },
+      }
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>l", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
     end,
   },
   {
@@ -89,4 +136,67 @@ return {
       { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
     },
   },
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local todo_comments = require "todo-comments"
+      todo_comments.setup()
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "folke/todo-comments.nvim",
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "folke/todo-comments.nvim",
+    },
+    opts = {
+      focus = true,
+    },
+    cmd = "Trouble",
+    keys = {
+      { "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>",              desc = "Open trouble workspace diagnostics" },
+      { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble document diagnostics" },
+      { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>",                 desc = "Open trouble quickfix list" },
+      { "<leader>xl", "<cmd>Trouble loclist toggle<CR>",                  desc = "Open trouble location list" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<CR>",                     desc = "Open todos in trouble" },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = function()
+      local conf = require "nvchad.configs.gitsigns"
+
+      conf.on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function opts(desc)
+          return { buffer = bufnr, desc = desc }
+        end
+        local map = vim.keymap.set
+
+        map("n", "<leader>gr", gs.reset_hunk, opts "Reset Hunk")
+        map("n", "<leader>gR", gs.reset_buffer, opts "Reset Buffer")
+        map("n", "<leader>gp", gs.preview_hunk, opts "Preview Hunk")
+        map("n", "<leader>gb", function() gs.blame_line { full = true } end, opts "Blame Line")
+        map("n", "<leader>gs", gs.stage_hunk, opts "Stage Hunk")
+        map("n", "<leader>gS", gs.stage_buffer, opts "Stage Buffer")
+        map("n", "<leader>gu", gs.undo_stage_hunk, opts "Undo Stage Hunk")
+        map("n", "<leader>gf", function() gs.diffthis('~1') end, opts "Diff This")
+        map("n", "<leader>gn", "<cmd>lua require('gitsigns').next_hunk()<cr>zz", opts "Next Hunk")
+        map("n", "<leader>tb", gs.toggle_current_line_blame, opts "Toggle Blame Line")
+      end
+      return conf
+    end
+  }
 }
